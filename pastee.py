28e92bc6e3c4a61f9ff61d38a141cb767caf7ba9
warren2i <83583153+Tensorshark@users.noreply.github.com>
2022-01-14 22:18:37 +0000

    Init

--- pastee.py
+++ pastee.py
@@ -1,6 +1,7 @@
 import requests
 from os.path import exists
-
+import json
+import time
 
 def configparser ():  ## checks if first launch, creates config file, asks for api key saves to file and in future loads from this
     file_exists = exists('config.cfg')
@@ -23,31 +24,106 @@ def configparser ():  ## checks if first launch, creates config file, asks for a
     return firstlaunch, apikey
 
 firstlaunch, apikey = configparser()
-configparser()
+
+def statuscheck(status):
+    status = status.status_code
+    if status == 200:
+        statusflag = True
+        statusmessage =  'success'
+    if status == 201:
+        statusflag = True
+        statusmessage = 'Paste was a success'
+    if status == 400:
+        statusflag = False
+        statusmessage =  'Bad Request – Your request sucks'
+    if status == 401:
+        statusflag = False
+        statusmessage =  'Unauthorized – Your Application/User application key is wrong.'
+    if status == 403:
+        statusflag = False
+        statusmessage =  'Forbidden – The application is a standard Application, and the resource requires a UserApplication.'
+    if status == 404:
+        statusflag = False
+        statusmessage =  'Not Found – The specified resource could not be found.'
+    if status == 405:
+        statusflag = False
+        statusmessage =  'Method Not Allowed – You tried to access an endpoint with an invalid method.'
+    if status == 406:
+        statusflag = False
+        statusmessage =  'Not Acceptable – You requested a format that isn’t json or xml.'
+    if status == 429:
+        statusflag = False
+        statusmessage =  'Too Many Requests – You’re submitting pastes too fast, slow down and try again later.'
+    if status == 500:
+        statusflag = False
+        statusmessage =  'Internal Server Error – Paste.ee had a problem with our server. Try again later.'
+    if status == 503:
+        statusflag = False
+        statusmessage =  'Service Unavailable – Paste.ee is temporarially offline for maintanance. Please try again later.'
+    print('Request status = '+statusmessage)
+    return statusflag, statusmessage
+
 
 
 def paste ( contents, name ):  ## make a new paste
     payload = {"description": name, "sections": [{"name": "Section1", "syntax": "autodetect", "contents": contents}]}
     headers = {'X-Auth-Token': apikey}
     post_response = requests.post(url = 'https://api.paste.ee/v1/pastes', json = payload, headers = headers)
-    print(post_response.text)
+    print(statuscheck(post_response)) ## returns the status of the request
+    if statuscheck(post_response)[0] is True:
+        print(post_response.text)
+        time.sleep(0.5)
+    else:
+        print('slowing down, we hit a wall')
+        time.sleep(5)
+#for x in range(30):
+#    paste('name','contents')
 
 
-# paste("this is the contents of the paste","pastename")
 
 def showpastes ():  ## returns json data array of pastes linked to api key
     headers = {'X-Auth-Token': apikey}
-    showpastes = requests.get(url = 'https://api.paste.ee/v1/pastes', headers = headers)
-    print(showpastes.text)
+    showpastes = requests.get(url = 'https://api.paste.ee/v1/pastes/', headers = headers)
+    print(showpastes.json())
+    for x in enumerate(showpastes.json()['data']):
+        print(x)
+    return showpastes.json()
 
 
-# showpastes()
+showpastes()
 
 def getpaste ( id ):  ## returns data as json for paste
     headers = {'X-Auth-Token': apikey}
     url = 'https://api.paste.ee/v1/pastes/' + id
     getpaste = requests.get(url = url, headers = headers)
-    print(getpaste.text)
+    if statuscheck(getpaste)[0] is True:
+        print(getpaste.json())
+    else: pass
+#getpaste('LzEcl')
+
+def deletepaste( id ):
+    headers = {'X-Auth-Token': apikey}
+    url = 'https://api.paste.ee/v1/pastes/' + id
+    deletepaste = requests.delete(url = url, headers = headers)
+    print(deletepaste.json())
+
+#deletepaste('TGPAw')
+
+def deleteallpastes():
+    temparr=showpastes()['data']
+    for x in enumerate(temparr): ## calls showpastes function, enumeate list, extract id and pass to delete paste function
+        id=(x[1]['id'])
+        deletepaste(id)
+        pass
+
+deleteallpastes()
+deleteallpastes()
+deleteallpastes()
+deleteallpastes()
+deleteallpastes()
+deleteallpastes()
+deleteallpastes()
+deleteallpastes()
 
 
-getpaste('HxwAl')
+
